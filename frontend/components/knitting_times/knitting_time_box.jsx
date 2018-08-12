c92e1b9e5fb723a4830b2a2fc05cc68d737e389e
Nataly Menares <nataly.menares@gmail.com>
2018-08-11 19:48:21 -0700

    Style knitting time boxes

--- frontend/components/knitting_times/knitting_time_box.jsx
+++ frontend/components/knitting_times/knitting_time_box.jsx
@@ -5,16 +5,20 @@ import moment from 'moment';
 function KnittingTimeBox(props) {
   const dateinfo = props.knittingtime.date
   return(
-    <div>
-      <li className="teatime">
+    <div className="kt-container">
+      <li className="kt">
         <h3>{moment(dateinfo).format('dddd')}</h3>
         <h1>{moment(dateinfo).format('MMM')} {moment(dateinfo).date()}</h1>
         <h2>{props.knittingtime.start_time} - {props.knittingtime.end_time}</h2>
-        <p>{props.knittingtime.address_1}, {props.knittingtime.address_2}, {props.knittingtime.city}, {props.knittingtime.zip},{props.knittingtime.state}</p>{props.knittingtime.address_1}
+        <p>{props.knittingtime.address_1} {props.knittingtime.city}</p>
       </li>
+      <div className="availability">
+        <h2>X seats left</h2>
+        <div className="progress">Progress Bar</div>
+      </div>
 
-      <div className="goteatime">
-        <h2>this one</h2>
+      <div className="go-kt">
+        <h2>this one →</h2>
       </div>
     </div>
 
